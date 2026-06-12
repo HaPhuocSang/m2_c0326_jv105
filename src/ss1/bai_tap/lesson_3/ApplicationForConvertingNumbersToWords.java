83beb187230f2295fa7f7e5e4cb11b9549d4638d
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-06-12 10:41:13 +0700

    fix ss1 lesson 3 and push ss2 practice

--- src/ss1/bai_tap/lesson_3/ApplicationForConvertingNumbersToWords.java
+++ src/ss1/bai_tap/lesson_3/ApplicationForConvertingNumbersToWords.java
@@ -5,303 +5,38 @@ import java.util.Scanner;
 public class ApplicationForConvertingNumbersToWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        int number;
-        try {
-            number = Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Not a valid number!");
-            return;
-        }
-        String result = "";
+        String[] ones = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] teens = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+        String[] tens = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
         if (number < 0 || number > 999) {
-            result = "out of ability";
+            System.out.println("out of ability");
         } else if (number < 10) {
-            switch (number) {
-                case 0:
-                    result = "zero";
-                    break;
-                case 1:
-                    result = "one";
-                    break;
-                case 2:
-                    result = "two";
-                    break;
-                case 3:
-                    result = "three";
-                    break;
-                case 4:
-                    result = "four";
-                    break;
-                case 5:
-                    result = "five";
-                    break;
-                case 6:
-                    result = "six";
-                    break;
-                case 7:
-                    result = "seven";
-                    break;
-                case 8:
-                    result = "eight";
-                    break;
-                case 9:
-                    result = "nine";
-                    break;
-            }
+            System.out.println(ones[number]);
         } else if (number < 20) {
-            switch (number) {
-                case 10:
-                    result = "ten";
-                    break;
-                case 11:
-                    result = "eleven";
-                    break;
-                case 12:
-                    result = "twelve";
-                    break;
-                case 13:
-                    result = "thirteen";
-                    break;
-                case 14:
-                    result = "fourteen";
-                    break;
-                case 15:
-                    result = "fifteen";
-                    break;
-                case 16:
-                    result = "sixteen";
-                    break;
-                case 17:
-                    result = "seventeen";
-                    break;
-                case 18:
-                    result = "eighteen";
-                    break;
-                case 19:
-                    result = "nineteen";
-                    break;
-            }
+            System.out.println(teens[number - 10]);
         } else if (number < 100) {
-            int dozens = number / 10;
-            int unitsDigit = number % 10;
-            switch (dozens) {
-                case 2:
-                    result = "twenty";
-                    break;
-                case 3:
-                    result = "thirty";
-                    break;
-                case 4:
-                    result = "forty";
-                    break;
-                case 5:
-                    result = "fifty";
-                    break;
-                case 6:
-                    result = "sixty";
-                    break;
-                case 7:
-                    result = "seventy";
-                    break;
-                case 8:
-                    result = "eighty";
-                    break;
-                case 9:
-                    result = "ninety";
-                    break;
-            }
-            if (unitsDigit != 0) {
-                switch (unitsDigit) {
-                    case 1:
-                        result += " one";
-                        break;
-                    case 2:
-                        result += " two";
-                        break;
-                    case 3:
-                        result += " three";
-                        break;
-                    case 4:
-                        result += " four";
-                        break;
-                    case 5:
-                        result += " five";
-                        break;
-                    case 6:
-                        result += " six";
-                        break;
-                    case 7:
-                        result += " seven";
-                        break;
-                    case 8:
-                        result += " eight";
-                        break;
-                    case 9:
-                        result += " nine";
-                        break;
-                }
-            }
+            int ten = number / 10;
+            int one = number % 10;
+            System.out.println((one != 0 ? tens[ten] + " " + ones[one] : tens[ten]));
         } else {
-            int hundreds = number / 100;
+            int hundred = number / 100;
             int remainder = number % 100;
-            switch (hundreds) {
-                case 1:
-                    result = "one hundred";
-                    break;
-                case 2:
-                    result = "two hundred";
-                    break;
-                case 3:
-                    result = "three hundred";
-                    break;
-                case 4:
-                    result = "four hundred";
-                    break;
-                case 5:
-                    result = "five hundred";
-                    break;
-                case 6:
-                    result = "six hundred";
-                    break;
-                case 7:
-                    result = "seven hundred";
-                    break;
-                case 8:
-                    result = "eight hundred";
-                    break;
-                case 9:
-                    result = "nine hundred";
-                    break;
-            }
+            String result = ones[hundred] + " hundred";
             if (remainder != 0) {
                 result += " and ";
-                if (remainder < 10) {
-                    switch (remainder) {
-                        case 1:
-                            result += "one";
-                            break;
-                        case 2:
-                            result += "two";
-                            break;
-                        case 3:
-                            result += "three";
-                            break;
-                        case 4:
-                            result += "four";
-                            break;
-                        case 5:
-                            result += "five";
-                            break;
-                        case 6:
-                            result += "six";
-                            break;
-                        case 7:
-                            result += "seven";
-                            break;
-                        case 8:
-                            result += "eight";
-                            break;
-                        case 9:
-                            result += "nine";
-                            break;
-                    }
+                if (remainder < 10){
+                    result += ones[remainder];
                 } else if (remainder < 20) {
-                    switch (remainder) {
-                        case 10:
-                            result += "ten";
-                            break;
-                        case 11:
-                            result += "eleven";
-                            break;
-                        case 12:
-                            result += "twelve";
-                            break;
-                        case 13:
-                            result += "thirteen";
-                            break;
-                        case 14:
-                            result += "fourteen";
-                            break;
-                        case 15:
-                            result += "fifteen";
-                            break;
-                        case 16:
-                            result += "sixteen";
-                            break;
-                        case 17:
-                            result += "seventeen";
-                            break;
-                        case 18:
-                            result += "eighteen";
-                            break;
-                        case 19:
-                            result += "nineteen";
-                            break;
-                    }
+                    result += teens[remainder - 10];
                 } else {
-                    int dozens = remainder / 10;
-                    int unitsDigit = remainder % 10;
-                    switch (dozens) {
-                        case 2:
-                            result += "twenty";
-                            break;
-                        case 3:
-                            result += "thirty";
-                            break;
-                        case 4:
-                            result += "forty";
-                            break;
-                        case 5:
-                            result += "fifty";
-                            break;
-                        case 6:
-                            result += "sixty";
-                            break;
-                        case 7:
-                            result += "seventy";
-                            break;
-                        case 8:
-                            result += "eighty";
-                            break;
-                        case 9:
-                            result += "ninety";
-                            break;
-                    }
-                    if (unitsDigit != 0) {
-                        switch (unitsDigit) {
-                            case 1:
-                                result += " one";
-                                break;
-                            case 2:
-                                result += " two";
-                                break;
-                            case 3:
-                                result += " three";
-                                break;
-                            case 4:
-                                result += " four";
-                                break;
-                            case 5:
-                                result += " five";
-                                break;
-                            case 6:
-                                result += " six";
-                                break;
-                            case 7:
-                                result += " seven";
-                                break;
-                            case 8:
-                                result += " eight";
-                                break;
-                            case 9:
-                                result += " nine";
-                                break;
-                        }
-                    }
+                    int ten = remainder / 10;
+                    int one = remainder % 10;
+                    result +=  one != 0 ? tens[ten] + " " + ones[one] : tens[ten];
                 }
             }
+            System.out.println(result);
         }
-        System.out.println(result);
     }
 }
